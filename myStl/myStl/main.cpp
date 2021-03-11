aa1dbd70f3e54956126532b929d1fd79d3bc572d
lukang <319428070@qq.com>
2021-03-11 12:42:19 +0800

    结构体的内存空间计算问题

--- myStl/myStl/main.cpp
+++ myStl/myStl/main.cpp
@@ -1,48 +1,20 @@
 #include<iostream>
 using namespace std;
-
-/*  游戏规则
-		1、有三根相邻的柱子，标号为A,B,C。
-		2、A柱子上从下到上按金字塔状叠放着n个不同大小的圆盘。
-		3、现在把所有盘子一个一个移动到柱子B上，并且每次移动同一根柱子上都不能出现大盘子在小盘子上方。
-*/
-
-//移动次数
-int count1=0;
-
-void move(const char A,const char C) 
+struct student
 {
-	count1++;
-	cout << A<<"->"<< C << endl;
-}
+	int num;
+	char name[20];
+	char gender;
+};
 
-//汉诺塔计算函数
-int hanNuoTa(int n,const char A, const char B, const char C)
+int main(int argc, char const *argv[])
 {
-	if (n == 1)
-	{
-		move(A, C);	
-		return 1;
-	}
-	else
-	{
-		hanNuoTa(n-1, A, C, B);
-		move(A, C);
-		hanNuoTa(n-1, B, A, C);
-	}
-	return 0;
-	
-
-}
-
-int main()
-{
-	int numDisks;//圆盘数量
-	cout << "input the number of disks" << endl;
-	cin >> numDisks;
-	hanNuoTa(numDisks, 'A', 'B', 'C');
-	cout << count1 << endl;
+	student s = { 10,"asd",'M' };
+	cout << s.num << endl;
+	cout << sizeof(s.num) << endl;
+	cout << sizeof(s.name) << endl;
+	cout << sizeof(s.gender) << endl;
+	cout << sizeof(s) << endl;
 	system("pause");
 	return 0;
-
 }
