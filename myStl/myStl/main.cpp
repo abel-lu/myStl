357c682e9d36a15eaa37194aff9d5c7fb801750d
lukang <319428070@qq.com>
2021-04-11 19:53:07 +0800

    评价问题

--- myStl/myStl/main.cpp
+++ myStl/myStl/main.cpp
@@ -1,60 +1,54 @@
 ﻿#include<iostream>
 #include<vector>
-#include<unordered_map>
-#include <numeric>
-#include <algorithm>
 using namespace std;
-//字符串 S 由小写字母组成。我们要把这个字符串划分为尽可能多的片段
-//同一字母最多出现在一个片段中。返回一个表示每个字符串片段的长度的列表。
 
-//输入：S = "ababcbacadefegdehijhklij"
-//输出：[9, 7, 8]
-//解释：
-//划分结果为 "ababcbaca", "defegde", "hijhklij"。
-//每个字母最多出现在一个片段中。
-//像 "ababcbacadefegde", "hijhklij" 的划分是错误的，因为划分的片段数较少。
 
-vector<int> cutstring(string s)
+int function(vector<vector<int>>& score)
 {
-	if (s.empty()) return vector<int>(0);
-	vector<int> cutvec;
-	unordered_map<char, int> map;
-	int size = s.size();
-	//用哈希map记录每个字符出现的最后位置
-	for (int i = 0; i<size; i++)
+	vector<int> grade(5, 0);
+	int max, min;
+	int n = score.size();
+	for (int i = 0; i < 5; i++)
 	{
-		map[s[i]] = i;
-	}
-	//map的key个数
-	//int s_count = map.size();
-	int loc= map[s[0]],pos=0,j=0;//划分字符串的位置
-	//cutvec.push_back(loc);//记录第一个划分位置
-	while(1)
-	{
-		for (j = pos; j < loc; j++)
+		//取每个维度的第一本书值
+		max = score[0][i];
+		for (int j = 0; j < n; j++)
 		{
-			if (map[s[j]] > loc)
+			//获得每个维度最大值
+			if (score[j][i] > max)
 			{
-				//更新当前字符段的最远位置
-				loc = map[s[j]];
+				max = score[j][i];
 			}
 		}
-		j++;
-		cutvec.push_back(loc-pos);
-		pos = loc;
-		loc = map[s[j]];
-		if (j == size) break;
+		grade[i] = max;
 	}
-	cutvec[0]++;
-	return cutvec;
+	min = grade[0];
+	for (int i = 0; i < 5; i++)
+	{
+		if (min > grade[i])
+		{
+			min = grade[i];
+		}
+	}
+	return min;
 }
 
 int main()
 {
-	vector<int> cutNum;
-	string S = "ababcbacadefegdehijhklij";
-	cutNum = cutstring(S);
-	int i = 0;
+	int n;
+	cin >> n;
+	if (n < 1) return 0;
+	//cout << n;
+	vector<vector<int>> score(n, vector<int>(5, 0));
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			cin >> score[i][j];
+		}
+	}
+	int grade = function(score);
+	cout << grade;
 	system("pause");
 	return 0;
 
