cffb253da2036181933de829c497e651bf767d51
lukang <319428070@qq.com>
2021-05-12 16:34:14 +0800

    fix

--- myStl/myStl/main.cpp
+++ myStl/myStl/main.cpp
@@ -5,37 +5,81 @@
 #include<algorithm>
 using namespace std;
 
-//给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
-//你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
-//返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+//给你一个长度为 n 的整数数组，请你判断在最多改变 1 个元素的情况下，该数组能否变成一个非递减数列。
+//
+//我们是这样定义一个非递减数列的： 对于数组中任意的 i(0 <= i <= n - 2)，总满足 nums[i] <= nums[i + 1]。
 
-//输入：[7, 1, 5, 3, 6, 4]
-//输出：5
-//解释：在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6 - 1 = 5 。
-//注意利润不能是 7 - 1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
 
+//输入: nums = [4, 2, 3]
+//	输出 : true
+//	解释 : 你可以通过把第一个4变成1来使得它成为一个非递减数列。
 
 
 class Solution {
 public:
-	int maxProfit(vector<int>& prices) {
-		int size = prices.size(), ans = 0;
-		int maxprice=0,minprice = prices[0];
-		for (int price : prices)
+	//方法一，遍历一遍，去掉扰乱数组的元素，再遍历一次剩下的数组
+	/*bool checkPossibility(vector<int>& nums) {
+		int size = nums.size();
+		if (size < 2) return true;
+		vector<int> v1,v2;
+		int num = -1;
+		v2.push_back(nums[0]);
+		for (int i = 1; i < size; i++)
 		{
-			maxprice = max(maxprice, price - minprice);
-			minprice = min(minprice, price);
+			if (nums[i] < nums[i-1])
+			{
+				num++;
+			}
+			else
+			{
+				v1.push_back(nums[i - 1]);
+				v2.push_back(nums[i]);
+			}
 		}
-		return maxprice;
+		if (num > 0) return false;
+		v1.push_back(nums[size - 1]); 
+		bool a=true,b=true;
+		for (int i = 1; i < v1.size(); i++)
+		{
+			if (v1[i] < v1[i - 1])
+				a = false;
+			if (v2[i] < v2[i - 1])
+				b = false;
+		}
+		return a || b;
+	}*/
+
+	//方法2，遍历一次，
+	bool checkPossibility(vector<int>& nums) {
+		int size = nums.size();
+		if (size < 2) return true;
+		int num = -1;
+		for (int i = 1; i < size; i++)
+		{
+			if (nums[i] < nums[i - 1])
+			{
+				num++;
+				
+				if(i>1&&(nums[i] < nums[i - 2]))
+				{
+					nums[i] = nums[i - 1];
+				}
+				/*else
+				{
+					nums[i - 1] = nums[i];
+				}*/
+			}
+		}	
+		return num<1;
 	}
 };
 
 int main()
 {
-	vector<int> people = { 7,1,5,3,6,4 };
-	int sortPeo;
+	vector<int> nums = { 4,2,3 };
+	bool sortPeo;
 	Solution solu;
-	sortPeo=solu.maxProfit(people);
+	sortPeo=solu.checkPossibility(nums);
 	cout << sortPeo<<endl;
 	system("pause");
 	return 0;
