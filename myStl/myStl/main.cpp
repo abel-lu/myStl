c3513759a435429e393dce0abbc3caa9a99dfc54
lukang <319428070@qq.com>
2021-05-19 14:40:04 +0800

    fix

--- myStl/myStl/main.cpp
+++ myStl/myStl/main.cpp
@@ -5,66 +5,42 @@
 #include<algorithm>
 using namespace std;
 
-//给定一个已按照 升序排列  的整数数组 numbers ，请你从数组中找出两个数满足相加之和等于目标数 target 。
-//函数应该以长度为 2 的整数数组的形式返回这两个数的下标值。
-//numbers 的下标 从 1 开始计数 ，所以答案数组应当满足 1 <= answer[0] < answer[1] <= numbers.length 。
+//给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+//
+//为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。
+//如果 pos 是 - 1，则在该链表中没有环。注意，pos 仅仅是用于标识环的情况，并不会作为参数传递到函数中。
 
 
+//输入：head = [3, 2, 0, -4], pos = 1
+//输出：返回索引为 1 的链表节点
+//解释：链表中有一个环，其尾部连接到第二个节点。
 
-//输入：numbers = [2, 7, 11, 15], target = 9
-//输出：[1, 2]
-//解释：2 与 7 之和等于目标数 9 。因此 index1 = 1, index2 = 2
+
+struct ListNode {
+    int val;
+	ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
 
 
 class Solution {
 public:
-
-	//方法一，暴力法，
-	//vector<int> twoSum(vector<int>& numbers, int target) {
-	//	vector<int> index(2,0);
-	//	int n = numbers.size();
-
-	//	for (int i = 0; i < n; i++)
-	//	{
-	//		for (int j = i+1; j < n; j++)
-	//		{
-	//			if (numbers[i] + numbers[j] == target)
-	//			{
-	//				index[0] = i;
-	//				index[1] = j;
-	//			}
-	//			if ((numbers[i] + numbers[j]) >target)
-	//			{
-	//				continue;
-	//				//j = i + 1;
-	//			}
-	//		}
-	//	}
-	//	return index;
-	//}
-
-	vector<int> twoSum(vector<int>& numbers, int target) {
-		vector<int> index(2,0);
-		int n = numbers.size();
-
-		for (int i = 0, j = n; i < n, j >-1; )
+	ListNode *detectCycle(ListNode *head) {
+		ListNode* fast = head;
+		ListNode* slow = head;
+		if (fast == NULL) return NULL;
+		do {
+			if ((fast->next == NULL) || (fast->next->next == NULL)) return NULL;
+			fast = fast->next->next;
+			slow = slow->next;
+		} while (fast != slow);
+		fast = head;
+		while (slow != fast)
 		{
-			if (numbers[i] + numbers[j] == target)
-			{
-				index[0] = i+1;
-				index[1] = j+1;
-				return index;
-			}
-			else if ((numbers[i] + numbers[j]) > target)
-			{
-				j--;
-			}
-			else
-			{
-				i++;
-			}
+			fast = fast->next;
+			slow = slow->next;
 		}
-		return vector<int>{-1,-1 };
+		return slow;
 	}
 
 };
@@ -74,7 +50,7 @@ int main()
 	vector<int> nums = { 1,2,3,4,4,9,56,90 };
 	vector<int> sortPeo;
 	Solution solu;
-	sortPeo=solu.twoSum(nums,8);
+	sortPeo=solu.detectCycle(nums,8);
 	//cout << sortPeo<<endl;
 	system("pause");
 	return 0;
