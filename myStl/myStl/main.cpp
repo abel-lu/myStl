c6c99c0d58a6cbdef295458dc82bb39fc475ccd2
lukang <319428070@qq.com>
2021-05-21 09:51:41 +0800

    fix

--- myStl/myStl/main.cpp
+++ myStl/myStl/main.cpp
@@ -9,70 +9,140 @@
 #include<opencv2/video/tracking.hpp>
 #include<opencv2/highgui/highgui.hpp>
 #include<time.h>
+#include<string.h>
 using namespace cv;
 using namespace std;
 using namespace cv::cuda;
 
 
 
-void convertFlowToImage(const Mat &flow, Mat &img_x, Mat &img_y,
-	double lowerBound, double higherBound) {
-#define CAST(v, L, H) ((v) > (H) ? 255 : (v) < (L) ? 0 : cvRound(255*((v) - (L))/((H)-(L))))
-	for (int i = 0; i < img_x.rows; ++i) {
-		for (int j = 0; j < img_x.cols; ++j) {
-			img_x.at<uchar>(i, j) = CAST(flow.at<Point2f>(i, j).x, lowerBound, higherBound);
-			img_y.at<uchar>(i, j) = CAST(flow.at<Point2f>(i, j).x, lowerBound, higherBound);
+using namespace cv;
+using namespace std;
+
+#define UNKNOWN_FLOW_THRESH 1e9
+
+void makecolorwheel(vector<Scalar> &colorwheel)
+{
+	int RY = 15;
+	int YG = 6;
+	int GC = 4;
+	int CB = 11;
+	int BM = 13;
+	int MR = 6;
+
+	int i;
+
+	for (i = 0; i < RY; i++) colorwheel.push_back(Scalar(255, 255 * i / RY, 0));
+	for (i = 0; i < YG; i++) colorwheel.push_back(Scalar(255 - 255 * i / YG, 255, 0));
+	for (i = 0; i < GC; i++) colorwheel.push_back(Scalar(0, 255, 255 * i / GC));
+	for (i = 0; i < CB; i++) colorwheel.push_back(Scalar(0, 255 - 255 * i / CB, 255));
+	for (i = 0; i < BM; i++) colorwheel.push_back(Scalar(255 * i / BM, 0, 255));
+	for (i = 0; i < MR; i++) colorwheel.push_back(Scalar(255, 0, 255 - 255 * i / MR));
+}
+
+void motionToColor(Mat flow, Mat &color)
+{
+	if (color.empty())
+		color.create(flow.rows, flow.cols, CV_8UC3);
+
+	static vector<Scalar> colorwheel; //Scalar r,g,b
+	if (colorwheel.empty())
+		makecolorwheel(colorwheel);
+
+	// determine motion range:
+	float maxrad = -1;
+
+	// Find max flow to normalize fx and fy
+	for (int i = 0; i < flow.rows; ++i)
+	{
+		for (int j = 0; j < flow.cols; ++j)
+		{
+			Vec2f flow_at_point = flow.at<Vec2f>(i, j);
+			float fx = flow_at_point[0];
+			float fy = flow_at_point[1];
+			if ((fabs(fx) >  UNKNOWN_FLOW_THRESH) || (fabs(fy) >  UNKNOWN_FLOW_THRESH))
+				continue;
+			float rad = sqrt(fx * fx + fy * fy);
+			maxrad = maxrad > rad ? maxrad : rad;
 		}
 	}
-#undef CAST
+
+	for (int i = 0; i < flow.rows; ++i)
+	{
+		for (int j = 0; j < flow.cols; ++j)
+		{
+			uchar *data = color.data + color.step[0] * i + color.step[1] * j;
+			Vec2f flow_at_point = flow.at<Vec2f>(i, j);
+
+			float fx = flow_at_point[0] / maxrad;
+			float fy = flow_at_point[1] / maxrad;
+			if ((fabs(fx) >  UNKNOWN_FLOW_THRESH) || (fabs(fy) >  UNKNOWN_FLOW_THRESH))
+			{
+				data[0] = data[1] = data[2] = 0;
+				continue;
+			}
+			float rad = sqrt(fx * fx + fy * fy);
+
+			float angle = atan2(-fy, -fx) / CV_PI;
+			float fk = (angle + 1.0) / 2.0 * (colorwheel.size() - 1);
+			int k0 = (int)fk;
+			int k1 = (k0 + 1) % colorwheel.size();
+			float f = fk - k0;
+			//f = 0; // uncomment to see original color wheel
+
+			for (int b = 0; b < 3; b++)
+			{
+				float col0 = colorwheel[k0][b] / 255.0;
+				float col1 = colorwheel[k1][b] / 255.0;
+				float col = (1 - f) * col0 + f * col1;
+				if (rad <= 1)
+					col = 1 - rad * (1 - col); // increase saturation with radius
+				else
+					col *= .75; // out of range
+				data[2 - b] = (int)(255.0 * col);
+			}
+		}
+	}
 }
-int main(int argc, char * argv[]) {
-	vector<Mat> flow;
-	//记得改文件路径
-	const char* path = "path_to_video";
-	Mat prev, curr, frame;
-	cv::Ptr<cv::optflow::DualTVL1OpticalFlow> alg_tvl1 = cv::optflow::DualTVL1OpticalFlow::create();
-	VideoCapture  capture(path);
-	capture.read(frame);
-	cvtColor(frame, prev, CV_BGR2GRAY);
-	int h = frame.rows;
-	int w = frame.cols;
-	if (!capture.isOpened()) {
-		cout << "Read video failed" << endl;
+
+int main(int, char**)
+{
+	VideoCapture cap;
+	//cap.open(0);
+	cap.open("D://FFOutput//原视频//normal0//右手捏手指.mp4");
+	string path = "D://BaiduNetdiskDownload//PD运动视频//";
+
+	if (!cap.isOpened())
 		return -1;
+
+	Mat prevgray, gray, flow, cflow, frame;
+	namedWindow("flow", 1);
+
+	Mat motion2color;
+	int x = 0;
+	for (;;)
+	{
+		double t = (double)cvGetTickCount();
+
+		cap >> frame;
+		cvtColor(frame, gray, CV_BGR2GRAY);
+		imshow("original", frame);
+		string n = std::to_string(x);
+		if (prevgray.data)
+		{
+			calcOpticalFlowFarneback(prevgray, gray, flow, 0.5, 3, 15, 3, 5, 1.2, 0);
+			motionToColor(flow, motion2color);
+			imshow("flow", motion2color);
+			cv::imwrite(path + n +".jpg", motion2color);
+		}
+		if (waitKey(10) >= 0)
+			break;
+		std::swap(prevgray, gray);
+		x++;
+		t = (double)cvGetTickCount() - t;
+		cout << "cost time: " << t / ((double)cvGetTickFrequency()*1000.) << endl;
 	}
-	clock_t begin, end;
-	begin = clock();
-	int frameNum = 2;
-	capture.set(CAP_PROP_POS_FRAMES, frameNum);
-	int num = 0;
-	while (capture.read(frame)) {
-		Mat d_flow;
-		cvtColor(frame, curr, CV_BGR2GRAY);
-		alg_tvl1->calc(prev, curr, d_flow);
-		flow.push_back(d_flow);
-		frameNum += 2; //每隔一帧计算光流
-		capture.set(CAP_PROP_POS_FRAMES, frameNum);
-		prev = curr.clone();
-		num++;
-	}
-	//记得改路径
-	char* save_dir = "save_path_for_flow";
-	int save_dir_len = strlen(save_dir);
-	for (int i = 0; i < flow.size(); i++) {
-		Mat img_x(h, w, CV_8UC1);
-		Mat img_y(h, w, CV_8UC1);
-		convertFlowToImage(flow[i], img_x, img_y, -15, 15);
-		char y_path[save_dir_len + 1 + 19];
-		sprintf(y_path, "%s%s%s%06d%s", save_dir, "/", "y_frame_", i, ".jpg");
-		char x_path[save_dir_len + 1 + 19];
-		sprintf(x_path, "%s%s%s%06d%s", save_dir, "/", "x_frame_", i, ".jpg");
-		imwrite(y_path, img_y);
-		imwrite(x_path, img_x);
-	}
-	end = clock();
-	cout << "total frames: " << num << endl;
-	cout << "time used: " << (double)(end - begin) / CLOCKS_PER_SEC << endl;
 	return 0;
 }
+
 
