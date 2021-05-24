219728f6e66d04be41bbe1169d93d554cc9fc1bd
lukang <319428070@qq.com>
2021-05-24 14:48:12 +0800

    fix

--- myStl/myStl/main.cpp
+++ myStl/myStl/main.cpp
@@ -4,84 +4,38 @@
 #include<unordered_map>
 #include <numeric>
 #include <algorithm>
+#include <math.h>
 using namespace std;
 
-//给你一个字符串 s 、一个字符串 t 。返回 s 中涵盖 t 所有字符的最小子串。如果 s 中不存在涵盖 t 所有字符的子串，则返回空字符串 "" 。
+////给定一个非负整数 c ，你要判断是否存在两个整数 a 和 b，使得 a^2 + b^2 = c 。
 //
-//注意：如果 s 中存在这样的子串，我们保证它是唯一的答案
-
-
-//输入：s = "ADOBECODEBANC", t = "ABC"
-//输出："BANC"
-//
-//输入：s = "a", t = "a"
-//输出："a"
+//输入：c = 5
+//输出：true
+//解释：1 * 1 + 2 * 2 = 5
 
 class solution
 {
 public:
-	string minWindow(string s, string t) {
-		int ss = s.size();
-		int tt = t.size();
-		if (ss == 0 || tt == 0 || ss < tt) return "";
-		unordered_map<char, int> map,need;
-		//用，map存储t
-		for (auto x : t)
+	bool judgeSquareSum(int c) {
+		if (c == 0) return true;
+		int s = sqrt(c);
+		long i, j;
+		while(i <= j)
 		{
-			map[x]++;
-		}
-		//need = map;
-		int left = 0, right = 0, pos;
-		int count=0,min=ss+1;
-		while (right < ss)
-		{
-			char a = s[right];
-			if (map[a] == 0)
+			if ((j*j + i*i) == c)
 			{
-
-				right++;
-				continue;
-				/*map[a]--;
-				pos = left;
-				dis=right-left;*/
+				return true;
 			}
-			if (need[a] < map[a])
+			else if ((j*j + i*i) > c)
 			{
-				count++;
+				j--;
 			}
-			need[a]++;
-			right++;
-
-			while (count == tt)
+			else
 			{
-				if (right - left < min)
-				{
-					min = right - left;
-					pos = left;
-				}
-
-				char b = s[left];
-				if (map[b] == 0)
-				{
-					left++;
-					continue;
-				}
-				if (need[b] == map[b])
-				{
-					count--;
-				}
-				need[b]--;
-				left++;
+				i++;
 			}
-			
-			
 		}
-		if (min == ss+1)
-		{
-			return "";
-		}
-		return s.substr(pos, min);
-		
+		return false;
 	}
 };
 
@@ -89,10 +43,8 @@ int main()
 {
 	solution solu;
 
-	string s = "ADOBECODEBANC";
-	string t = "ABC";
-
-	string result=solu.minWindow(s,t);
+	int x = 5;
+	bool result=solu.judgeSquareSum(x);
 	cout << result << endl;
 	system("pause");
 	return 0;
