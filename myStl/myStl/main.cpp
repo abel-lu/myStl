488e8e3bb29f625ca4ed5972b705b202a7a9adc8
康 陆 <lukanghuaidan@outlook.com>
2021-06-23 11:22:42 +0800

    fix

--- myStl/myStl/main.cpp
+++ myStl/myStl/main.cpp
@@ -7,52 +7,51 @@
 #include <math.h>
 using namespace std;
 
-
-//给定一个字符串，请将字符串里的字符按照出现的频率降序排列。
-
-//输入:
-//"tree"
 //
-//输出 :
-//	"eert"
+//给定一个包含红色、白色和蓝色，一共 n 个元素的数组
+//原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+
+//此题中，我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
+
+//输入：nums = [2, 0, 2, 1, 1, 0]
+//输出：[0, 0, 1, 1, 2, 2]
 
 class solution
 {
 public:
 
 	//方法1暴力
-	string frequencySort(string& s) {
-		unordered_map<char,int> map;
-		int size = s.size();
-		int max;
-		if (size < 2) return s;
-		max = 0;
-		for(auto a:s)
+	void sortColors(vector<int>& nums) {
+		int size = nums.size(); 
+		if (size < 2) return;
+		int count[3] = {0};
+		for (auto x : nums)
 		{
-			map[a]++;
-		}
-		
-		vector<char>* res= new vector<char>[s.length()+1];
-		//vector<char> res[3];
-		for (auto a: map)
-		{
-			int x = a.second;
-			res[x].push_back(a.first);
-		}
-
-		string ans;
-		int n = s.length();
-		for (int i = n; i >0 ; i--)
-		{
-			for (int j = 0; j < res[i].size(); j++)
+			if (x == 0)
 			{
-				for (int k = 0; k < i; k++)
-				{
-					ans += res[i][j];
-				}
+				count[0]++;
+			}
+			else if (x == 1)
+			{
+				count[1]++;
+			}
+			else
+			{
+				count[2]++;
 			}
 		}
-		return ans;
+		vector<int> res;
+		int i = 0;
+		while (i<3)
+		{
+			while (count[i]--)
+			{
+				res.push_back(i);
+			}
+			i++;
+		}
+		nums = res;
+		return;
 	}
 };
 
@@ -60,10 +59,10 @@ int main()
 {
 	solution solu;
 
-	vector<int> nums = { 1,2,1 };
+	vector<int> nums = { 2,0,2,1,1,0 };
 	string str = "eeeee";
 	int target = 0;
-	string result = solu.frequencySort(str);
+	solu.sortColors(nums);
 	//cout << result << endl;
 
 	system("pause");
